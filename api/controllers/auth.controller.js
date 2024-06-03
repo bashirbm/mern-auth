ebc02fafe6fe875838a4b27d3b0c76b4122a823b
bashirbm <bashirmdbm@gmail.com>
2024-06-03 21:14:57 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -27,6 +27,6 @@ export const signin = async (req, res, next) => {
     const expiryDate = new Date(Date.now() + 3600000); //1 hour
     res.cookie('access_token', token, { httpOnly: true, expires: expiryDate}).status(200).json(rest)
   } catch (error) {
-    next(error); 
+    next(error);
   }
 };
